1be42dac09a89c1552d04687a1710b7d07f736de
Ben Steinbook <ben.steinbook@gmail.com>
2015-02-24 13:05:59 -0700

    commit all my stuff

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,13 +6,18 @@ class PeopleController < ApplicationController
 
   def new
     @person = Person.new
+
   end
 
   def create
     @person = Person.new(person_params)
-    @person.save
+    if @person.save
     redirect_to people_path, notice: "Person saved successfully"
+else
+  render :new
+end
   end
+
 
   def edit
     @person = Person.find(params[:id])
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,7 @@
 class Person < ActiveRecord::Base
 
+  validates :first_name, presence: true
+
 
 
 end
